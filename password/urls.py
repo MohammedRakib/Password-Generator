390c6994cb577fe23d93883b614fce4f12b8b09c
Mohammed Rakib <rafz.rakib@gmail.com>
2020-07-23 01:36:50 +0600

    l

--- password/urls.py
+++ password/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home,name='home'),
     path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('password/', views.password, name='password'),
