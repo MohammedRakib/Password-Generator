2859536ac59d06584b7075cc2108eacd983dea83
Mohammed Rakib <rafz.rakib@gmail.com>
2020-07-22 21:38:47 +0600

    created homepage and password

--- password/views.py
+++ password/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'password/homepage.html')
+
+
+def password(request):
+    return render(request, 'password/password.html')
